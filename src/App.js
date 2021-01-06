8261b0166243afb6f61c91310f9552979d85da58
Kishor-Dure <dure.kb@gmail.com>
2021-01-07 00:33:45 +0530

    create a project using React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from '../src/Component/Pages/Homepages/homepage.component';
+
+function App() {
+  return (
+    <div >
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
